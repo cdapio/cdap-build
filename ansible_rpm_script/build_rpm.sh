04c1da48c21e965c4ae33acd01eeed185df793fd
shashank <shashank.jain@guavus.com>
2018-11-15 12:49:30 +0530

    RAFD-29: creating rpm for the installer to run cdap and cdap aceelerator plugins installer

--- ansible_rpm_script/build_rpm.sh
+++ ansible_rpm_script/build_rpm.sh
@@ -2,11 +2,9 @@ set -e
 
 DIST_DIR_INSTALLER="ansible_rpm/"
 
-rm -rf ansible_rpm/*
 VERSION="5.1"
 DATE=`date +'%Y%m%d'`
 
 echo -e "# # # # # # # START : Creating RPM package Solution Installer # # # # # # #"
 fpm -f -s dir -t rpm --rpm-os linux -v ${VERSION} --iteration ${DATE} --chdir ansible -p $DIST_DIR_INSTALLER -n ansible_rpm .
 echo -e "# # # # # # # # END : Creating RPM package for Solution Installer # # # # # # #"
-
